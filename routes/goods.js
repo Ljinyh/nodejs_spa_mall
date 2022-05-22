7e348036d35f8878963e16270becf3f67b345975
Ljinyh <com15643@naver.com>
2022-05-23 01:10:07 +0900

    첫 커밋

---
+++ routes/goods.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const Goods = require("../schemas/goods");
+const Cart = require("../schemas/cart");
+const router = express.Router();
+
+//페이지에 글자 적기
+router.get("/", (req, res) => {
+  res.send("this is root page");
+});
+
+//상품에 대한것 가져오기
+router.get("/goods", async (req, res) => {
+  const { category } = req.query;
+
+  const goods = await Goods.find({ category });
+
+  res.json({
+    goods,
+  });
+});
+
+//장바구니에 어떤 상품이 들어가있는지 보여주기
+router.get("/goods/:goodsId", async (req, res) => {
+  const { goodsId } = req.params;
+
+  const [detail] = await Goods.find({ goodsId: Number(goodsId) });
+
+  res.json({
+    detail,
+  });
+});
+
+
+//장바구니에 상품 추가하기
+router.post("/goods/:goodsId/cart", async (req, res) => {
+  const { goodsId } = req.params;
+  const { quantity } = req.body;
+
+  const existsCarts = await Cart.find({ goodsId: Number(goodsId) });
+  if (existsCarts.length) {
+    return res.status(400).json({ success: false, errorMessage: "이미 장바구니에 존재하는 상품입니다." });
+  }
+
+
+ await Cart.create({ goodsId: Number(goodsId), quantity: quantity });
+ res.json({ success: true });
+});
+
+
+//장바구니에서 삭제하기
+router.delete("/goods/:goodsId/cart", async (req,res) => {
+  const { goodsId } = req.params;
+
+
+  const existsCarts = await Cart.find({ goodsId: Number(goodsId) });
+  if (existsCarts.length) {
+    await Cart.deleteOne({ goodsId: Number(goodsId) });
+  }
+  res.json({ success: true });
+});
+
+//장바구니에서 상품 수정하기
+router.put("/goods/:goodsId/cart", async (req, res) => {
+  const { goodsId } = req.params;
+  const { quantity } = req.body;
+
+ const existsCarts = await Cart.find({ goodsId: Number(goodsId) });
+  if (!existsCarts.length) {
+    return res.status(400).json({ success: false, errorMessage: "장바구니에 해당 상품이 없습니다." });
+  }
+
+  await Cart.updateOne({ goodsId: Number(goodsId)}, { $set: { quantity }});
+
+  res.json({ success: true });
+});
+
+//상품이 현재 있는 상품인지 판별하기, 없으면 가져오기
+router.post("/goods", async (req, res) => {
+  const { goodsId, name, thumbnailUrl, category, price } = req.body;
+
+  const goods = await Goods.find({ goodsId });
+  if (goods.length) {
+    return res.status(400).json({ success: false, errorMesssage: "이미 있는 데이터입니다." });
+  }
+
+  const createdGoods = await Goods.create({ goodsId, name, thumbnailUrl, category, price });
+
+  res.json({ goods: createdGoods });
+});
+
+module.exports = router;
